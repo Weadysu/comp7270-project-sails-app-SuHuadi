2cdae5052812640a8627564794650fe1b03bd18a
SuHuadi <19403372@life.hkbu.edu.hk>
2019-11-05 12:44:25 +0800

    Added initial data to bootstrap.js.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -27,20 +27,54 @@ module.exports.bootstrap = async function () {
   // ]);
   // ```
 
-  if (await Rental.count() > 0) {
-    return;
-  }
+  sails.bcrypt = require('bcryptjs');
+  const saltRounds = 10;
 
-  await Rental.createEach([
-    { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Hmlet_Foo_Ming_1.jpg", "estate": "Festival City", "bedrooms": "2", "grossArea": 800, "expectedTenants": "2", "rent": 15000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/drive/68bb2a740af71ee7b4588c5b8416f63295c620d8.jpeg", "estate": "City One Shatin", "bedrooms": "2", "grossArea": 250, "expectedTenants": "2", "rent": 28000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/drive/f84d452a9f142fb0f644c188337e6ca6465849e6.jpeg", "estate": "Festival City", "bedrooms": "2", "grossArea": 150, "expectedTenants": "2", "rent": 12000, "box": "highLighted", },
-    { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_4T79eYs.jpg", "estate": "Tin Ma Court", "bedrooms": "2", "grossArea": 150, "expectedTenants": "2", "rent": 13000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/proxy/01_a.jpg", "estate": "Festival City", "bedrooms": "3", "grossArea": 600, "expectedTenants": "2", "rent": 25000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_3CjRdrh.jpg", "estate": "Tin Ma Court", "bedrooms": "4", "grossArea": 900, "expectedTenants": "2", "rent": 23000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Hmlet_33_Centre_Street_1.jpg", "estate": "Festival City", "bedrooms": "3", "grossArea": 550, "expectedTenants": "2", "rent": 24000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_Zu1YAxb.jpg", "estate": "Tin Ma Court", "bedrooms": "2", "grossArea": 600, "expectedTenants": "3", "rent": 24000, "box": "highLighted" },
-    { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail-_1_uMIU89E.jpg", "estate": "City One Shatin", "bedrooms": "2", "grossArea": 800, "expectedTenants": "2", "rent": 24000, "box": "highLighted" },
-  ]);
-  
+  if (await Rental.count() == 0) {
+
+    await Rental.createEach([
+      { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Hmlet_Foo_Ming_1.jpg", "estate": "Festival City", "bedrooms": "2", "grossArea": 800, "expectedTenants": "2", "rent": 15000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/drive/68bb2a740af71ee7b4588c5b8416f63295c620d8.jpeg", "estate": "City One Shatin", "bedrooms": "2", "grossArea": 250, "expectedTenants": "2", "rent": 28000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/drive/f84d452a9f142fb0f644c188337e6ca6465849e6.jpeg", "estate": "Festival City", "bedrooms": "2", "grossArea": 150, "expectedTenants": "2", "rent": 12000, "box": "highLighted", },
+      { "createDate": "10/9/2019", "updateDate": "10/27/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_4T79eYs.jpg", "estate": "Tin Ma Court", "bedrooms": "2", "grossArea": 150, "expectedTenants": "2", "rent": 13000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/proxy/01_a.jpg", "estate": "Festival City", "bedrooms": "3", "grossArea": 600, "expectedTenants": "2", "rent": 25000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_3CjRdrh.jpg", "estate": "Tin Ma Court", "bedrooms": "4", "grossArea": 900, "expectedTenants": "2", "rent": 23000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Hmlet_33_Centre_Street_1.jpg", "estate": "Festival City", "bedrooms": "3", "grossArea": 550, "expectedTenants": "2", "rent": 24000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail_1_Zu1YAxb.jpg", "estate": "Tin Ma Court", "bedrooms": "2", "grossArea": 600, "expectedTenants": "3", "rent": 24000, "box": "highLighted" },
+      { "createDate": "10/9/2019", "updateDate": "10/15/2019", "propertyTitle": "Description", "imageURL": "https://cdn-hmlet.s3-ap-southeast-1.amazonaws.com/buildings/Thumbnail-_1_uMIU89E.jpg", "estate": "City One Shatin", "bedrooms": "2", "grossArea": 800, "expectedTenants": "2", "rent": 24000, "box": "highLighted" },
+    ]);
+
+  };
+
+  return generateUsers();
+
+  async function generateUsers() {
+
+    if (await User.count() > 0) {
+      return;
+    }
+
+    const hash = await sails.bcrypt.hash('123456', saltRounds);
+
+    await User.createEach([
+      { username: "admin", password: hash, role: "admin" },
+      { username: "boss", password: hash, },
+      // { password: hash },
+      // etc.
+    ]);
+
+    const martin = await Rental.findOne({ name: "Martin Choy" });
+    const kenny = await Rental.findOne({ name: "Kenny Cheng" });
+    const admin = await User.findOne({ username: "admin" });
+    const boss = await User.findOne({ username: "boss" });
+
+    await User.addToCollection(admin.id, 'supervises').members(kenny.id);
+    await User.addToCollection(boss.id, 'supervises').members([martin.id, kenny.id]);
+
+  };
+
 };
+
+
+
+
